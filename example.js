1033c913c3c9dba932c65b817ddf4aa15091ec9c
Ivan <ivan@plugion.org>
2021-05-03 14:53:28 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
-var special_dates = '05/13/2021,05/14/2021';
+var special_dates = '05/13/2021;05/14/2021';
 var spevial_time_text = 'your custom text';
 
 jQuery(document).on('wbk_timeslots_rendered', function( event, container ){
-   special_dates_arr = special_dates.split(',');
+   special_dates_arr = special_dates.split(';');
    var cur_date = jQuery('#wbk-slots-container').find('.wbk-day-title').html().trim();
    
    if(special_dates_arr.indexOf(cur_date) != -1){
